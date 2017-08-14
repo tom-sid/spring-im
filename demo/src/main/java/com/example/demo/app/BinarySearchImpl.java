0132ea11ef20bf9ea4009f91cacd5bc7f7d4931b
Reji <Reji@DESKTOP-5T8LV75>
2017-08-14 17:36:41 -0500

    Setter injection

--- demo/src/main/java/com/example/demo/app/BinarySearchImpl.java
+++ demo/src/main/java/com/example/demo/app/BinarySearchImpl.java
@@ -9,10 +9,6 @@ public class BinarySearchImpl {
 	@Autowired
 	private SortAlgorithm sortAlgorithm;
 
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		this.sortAlgorithm = sortAlgorithm;
-	}
-
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		int[] sortedNumbers = sortAlgorithm.sort(numbers);
 		System.out.println(sortAlgorithm + " : " + sortedNumbers);
